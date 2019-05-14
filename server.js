0933b347de741fff1c01f12a5814f5a5d91d16c7
JVB <jvb@JMacBook-Air.home>
2019-05-13 21:24:28 -0400

    added html/css sign up and login file

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "Vash");
+
+// handlers
+// function aboutHandler(req, res) {
+//     // make db call
+//     // take data render Vash file passing said data in
+//     // res.render("index",{name: "Peeky"});
+//     res.render("index");
+// }
+
+// function homeHandler(req, res) {
+//     res.render("")
+// }
+
+function loginHandler(req, res) {
+    res.render("login");
+}
+
+
+function signupHandler(req, res) {
+    res.render("signup");
+}
 // endpoints
+//app.get("/about", aboutHandler);
 
-function aboutHandler(req, res) {
-    res.json({message: "GO AWAY!!!"})
-}
+//app.get("/home", homeHandler);
 
-function homeHandler(req, res) {
-    res.json({message: "Feel free to leave ma'am!"})
-}
-
-app.get("/about", aboutHandler);
-
-app.get("/home", homeHandler);
+app.get("/login", loginHandler);
 
 // function whenRunning() {
 //     console.log("server is running on port 5000");
 // }
+app.get("/signup", signupHandler);
+
 
 // app.listen(5000, whenRunning);
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+function disp() {
+    console.log([i])
+}
+
+
